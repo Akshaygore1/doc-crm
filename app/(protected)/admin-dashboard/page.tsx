605d87abb2a844acfd4ae59379d76360358a29cf
Akshaygore1 <akshaygore2301@gmail.com>
2025-04-25 15:23:09 +0530

    Implement loading state and error handling in Admin Dashboard user fetch. Display loading indicator while fetching users and show error toast on fetch failure.

--- app/(protected)/admin-dashboard/page.tsx
+++ app/(protected)/admin-dashboard/page.tsx
@@ -3,20 +3,28 @@ import { Button } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
 import { DialogDemo } from "@/components/add-user-modal";
 import { useEffect, useState } from "react";
-import { getUsers } from "@/lib/user";
 import UsersTable from "@/components/user-table";
+import { toast } from "sonner";
 
 export default function AdminDashboard() {
   const { data: session } = useSession();
   const [open, setOpen] = useState(false);
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const users = await fetch("/api/auth/users");
-      const data = await users.json();
-      console.log(data);
-      setUsers(data);
+      try {
+        setLoading(true);
+        const users = await fetch("/api/auth/users");
+        const data = await users.json();
+        setUsers(data);
+      } catch (error) {
+        toast.error("Failed to fetch users");
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchUsers();
   }, []);
@@ -30,7 +38,15 @@ export default function AdminDashboard() {
         </div>
       </div>
       <div className="p-4">
-        <UsersTable users={users} />
+        {loading ? (
+          <div className="flex justify-center py-10 items-center h-full">
+            <div className="space-y-4 w-full">
+              <div className="h-48 w-full bg-gray-200 animate-pulse rounded-md" />
+            </div>
+          </div>
+        ) : (
+          <UsersTable users={users} />
+        )}
       </div>
       <DialogDemo open={open} setOpen={setOpen} />
     </div>
